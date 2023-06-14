4c156b1b5b67cabb5962eb223cfa9264573ad076
Barry Martin <bymartin@users.noreply.github.com>
2023-06-14 16:52:10 -0500

    GetComponent, change color of car when picking up/delivery packages

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(0, 255, 0, 255);
+    [SerializeField] Color32 noPackageColor = new Color32(255, 255, 255, 255);
     [SerializeField] float destroyDelay = 0.5f;
     bool hasPackage = false;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -17,10 +26,12 @@ public class Delivery : MonoBehaviour
         if (collision.tag == "Package" && !hasPackage) {
             Debug.Log("Package picked up");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(collision.gameObject, destroyDelay);
         }
         if (collision.tag == "Customer" && hasPackage) {
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
             Debug.Log("Package delivered!");
         }
 
